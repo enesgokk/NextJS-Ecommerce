566a3f25ecb1ca5e564f08b88152f92d7103d695
enesgokk <enes_sare@hotmail.com>
2021-07-30 13:54:52 +0300

    basketSide https://next-js-ecommerce-enesgokk.vercel.app/

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,21 @@
 import Header from "../../components/Header";
+import CheckoutProduct from "../../components/CheckoutProduct";
 import Image from "next/image"
+import { useSelector } from "react-redux";
+import { selectItems, selectTotal } from "../slices/basketSlice";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/client";
 
 function Checkout() {
+	const items=useSelector(selectItems);
+	const total=useSelector(selectTotal);
+	const [session]=useSession();
+
 	return (
 		<div className="gray-1">
 			<Header/>
 			
-			<main className="lg:flex max-w-screen-2xl mx-auto">
+			<main className="checkFlex">
 				{/*Left*/}
 				<div className="flex-grow m-5 shadow-sm">
 					<Image
@@ -16,13 +25,51 @@ function Checkout() {
 					objectFit="contain"
 					/>
 				
-
 					<div className="flex flex-col p-4 space-y-10 bg-white">
-						<h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+						<h1 className="text-3xl border-b pb-4">
+							{items.length ===0 
+							? "Your amazon basket is empty."
+							: "Shopping Basket" }
+						</h1>
+
+							{items.map((item,i)=>(
+								<CheckoutProduct
+									key={i}
+									id={item.id}
+									title={item.title}
+									price={item.price}
+									rating={item.rating}
+									description={item.description}
+									category={item.category}
+									image={item.image}
+									hasPrime={item.hasPrime}
+								/>
+							))}
+
 					</div>
 				</div>
 
 				{/*Right*/}
+				<div className="flex flex-col bg-white p-10 shadow-md">
+					{items.length > 0 && (
+						<>
+							<h2 className="whitespace-nowrap">
+								Subtotal ({items.length} items):{" "}
+								<span className="font-bold">
+									<Currency quantity={total} currency="GBP"/>
+								</span>
+							</h2>
+
+							<button
+							disabled={!session} 
+							className={`button mt-2 ${
+								!session && "disabledButton"}`}
+							>
+								{!session ? 'Sign in to checkout': 'Proceed to checkout'}
+							</button>
+						</>
+					)}
+				</div>
 			</main>
 		</div>
 	)
